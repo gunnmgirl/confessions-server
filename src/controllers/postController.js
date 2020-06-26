7020e1ef229d377a4ed1fd8a3a9e907e602ad8ab
gunnmgirl <48892214+gunnmgirl@users.noreply.github.com>
2020-06-26 09:55:32 +0200

    Adds server-side validation using Yup

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -1,41 +1,30 @@
+import * as Yup from "yup";
+
 import Post from "../models/post";
 
+const validationSchema = Yup.object().shape({
+  content: Yup.string()
+    .max(1000, "Must be less than 1000 characters")
+    .min(10, "Must me at least 10 characters")
+    .required("Required!"),
+});
+
 async function postPost(req, res, next) {
-  const text = req.body.text;
-  const upvotes = req.body.upvotes;
-  const downvotes = req.body.downvotes;
-  const comments = req.body.comments;
-  const post = new Post({
-    text: text,
-    upvotes: upvotes,
-    downvotes: downvotes,
-    comments: comments,
-  });
   try {
-    await post.save();
-    return res.status(200).json({
-      success: true,
-      message: "Post created :D",
-    });
-  } catch (err) {
-    return res.status(400).json({
-      error: err,
-      message: "Could not create a post :(",
-    });
+    await validationSchema.validate(req.body.text, { abortEarly: false });
+    const post = await Post.create(req.body);
+    res.status(200).send(post);
+  } catch (error) {
+    res.status(400).send(error);
   }
 }
 
 async function getPosts(req, res, next) {
   try {
     const posts = await Post.find();
-    return res
-      .status(200)
-      .json({ success: true, posts, message: "Got all posts :)" });
-  } catch (err) {
-    return res.status(400).json({
-      error: err,
-      message: "Could not get posts :'(",
-    });
+    return res.status(200).send(posts);
+  } catch (error) {
+    res.status(400).send(error);
   }
 }
 
