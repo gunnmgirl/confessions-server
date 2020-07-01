b9381e43bbe70c9fee85a5f9644e312ec06c7ded
gunnmgirl <48892214+gunnmgirl@users.noreply.github.com>
2020-07-01 09:52:22 +0200

    Adds getPostById controller function

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -1,6 +1,6 @@
 import Post from "../models/post";
 
-function validate(content) {
+function validatePost(content) {
   if (
     typeof content !== "string" ||
     content.length < 10 ||
@@ -11,14 +11,43 @@ function validate(content) {
   return true;
 }
 
+function validateComment(content) {
+  if (
+    typeof content !== "string" ||
+    content.length < 5 ||
+    content.length > 500
+  ) {
+    return false;
+  }
+  return true;
+}
+
 async function postPost(req, res, next) {
   const content = req.body.text;
   try {
-    if (!validate(content)) {
+    if (!validatePost(content)) {
       return res.status(400).send("Validation failed");
     }
     const post = await Post.create(req.body);
     res.status(200).send(post);
+  } catch (error) {
+    return res.status(400).send(error);
+  }
+}
+
+async function postComment(req, res, next) {
+  const content = req.body.comment;
+  const id = req.body.id;
+  try {
+    if (!validateComment(content)) {
+      return res.status(400).send("Validation failed");
+    }
+    await Post.updateOne({ _id: id }, { $push: { comments: content } });
+    const result = {
+      content,
+      id,
+    };
+    res.status(200).send(result);
   } catch (error) {
     return res.status(400).send(error);
   }
@@ -33,4 +62,13 @@ async function getPosts(req, res, next) {
   }
 }
 
-export { postPost, getPosts };
+async function getPostById(req, res, next) {
+  try {
+    const post = await Post.findById(req.params.id);
+    return res.status(200).send(post);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+}
+
+export { postPost, getPosts, getPostById, postComment };
--- src/routes/postRoutes.js
+++ src/routes/postRoutes.js
@@ -1,10 +1,17 @@
 import express from "express";
 
-import { postPost, getPosts } from "../controllers/postController";
+import {
+  postPost,
+  getPosts,
+  getPostById,
+  postComment,
+} from "../controllers/postController";
 
 const router = express.Router();
 
 router.post("/", postPost);
+router.post("/:id", postComment);
 router.get("/", getPosts);
+router.get("/:id", getPostById);
 
 export default router;
